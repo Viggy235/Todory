05bf2fa5adc83ce1f1f98ef72a560fcf0b4175a5
Vignesh S <viggy235@gmail.com>
2019-09-14 16:02:18 +0530

    Initial Commit

---
+++ Todory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todory
+//
+//  Created by Vignesh S on 14/09/19.
+//  Copyright © 2019 Vignesh S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
